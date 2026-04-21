0b5b7777663e545507497d1984a23f39f2db66e8
nightsky-tech <astrokrithin@gmail.com>
2026-04-21 09:51:41 +0530

    UC2: toss to decide first player and assign symbols

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.Random;
+
 public class App {
 
     public static void main(String[] args) {
@@ -7,8 +9,15 @@ public class App {
 
         initializeBoard(board);
         printBoard(board);
+
+        GameState game = tossAndAssign();
+
+        System.out.println("Player Symbol: " + game.playerSymbol);
+        System.out.println("Computer Symbol: " + game.computerSymbol);
+        System.out.println("First Turn: " + game.currentPlayer);
     }
 
+    // UC1 methods
     public static void initializeBoard(char[][] board) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -25,5 +34,32 @@ public class App {
             }
             System.out.println();
         }
+    }
+
+    // UC2 logic
+    public static GameState tossAndAssign() {
+        Random rand = new Random();
+        GameState game = new GameState();
+
+        int toss = rand.nextInt(2);
+
+        if (toss == 0) {
+            game.currentPlayer = "Player";
+            game.playerSymbol = 'X';
+            game.computerSymbol = 'O';
+        } else {
+            game.currentPlayer = "Computer";
+            game.playerSymbol = 'O';
+            game.computerSymbol = 'X';
+        }
+
+        return game;
+    }
+
+    // Game state class
+    static class GameState {
+        char playerSymbol;
+        char computerSymbol;
+        String currentPlayer;
     }
 }
